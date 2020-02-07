e07670eaeaf2bd294f15ae8300fe5fd9962f17f6
PAPIRATA-RD <enmanuelm15@hotmail.com>
2020-02-07 12:02:52 -0400

    Add files via upload

---
+++ numeros_invertidos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace numeros_invertidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numeros invertidos \n");
+
+            int num1, num2, num3;
+            Console.WriteLine("Numero invertido");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            string num = num1.ToString();//RECORDAR PARA CONVERTIR UN INT PARA STRING PRIMERO TENGO QUE DECLARAR EL STRING QUE QUIERO
+            char[] invertir = num.ToCharArray();
+            Array.Reverse(invertir);
+
+            Console.WriteLine(invertir);
+            Console.ReadKey();
+
+            
+         }
+    }
+}
